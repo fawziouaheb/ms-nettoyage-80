c7869154b900e6c94a0507971d449a383a550549
Fawzi OUAHEB <113803014+fawziouaheb@users.noreply.github.com>
2024-12-26 16:53:06 +0100

    modification en cours pour le deployement

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import { DisponibilidadComponent } from "../disponibilidad/disponibilidad.component";
 import { ContactComponent } from "../contact/contact.component";
-import { NavbarComponent } from "../navbar/navbar.component";
 import { ServiceComponent } from "../service/service.component";
 import { AnimationTextComponent } from "../animation-text/animation-text.component";
 import { PresentationMsComponent } from "../presentation-ms/presentation-ms.component";
 
 @Component({
   selector: 'app-home',
-  imports: [FooterComponent, DisponibilidadComponent, ContactComponent, NavbarComponent, ServiceComponent, AnimationTextComponent, PresentationMsComponent],
+  imports: [FooterComponent, DisponibilidadComponent, ContactComponent, ServiceComponent, AnimationTextComponent, PresentationMsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
